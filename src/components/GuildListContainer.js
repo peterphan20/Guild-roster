381b00c33d1bf33688e9d91741d1c9e91d9f6bf5
Peter Phan <peterphan0422@gmail.com>
2021-04-10 23:59:30 -0500

    feat: further page styling

--- src/components/GuildListContainer.js
+++ src/components/GuildListContainer.js
@@ -129,6 +129,15 @@ const GuildListContainer = ({
 						personal project for learning purposes and is not accepting contributions. You are
 						welcome to modify and distribute any versions as you please. 🦕
 					</p>
+					<StyledApplyNowContainer>
+						<h1>Ready for an adventure?</h1>
+						<p>If you want to make WOW history, join us on the road to world first.</p>
+						<Link to="/signup">
+							<StyledApplyNowButton>
+								<Link to="/signup">Apply Now ➡</Link>{" "}
+							</StyledApplyNowButton>
+						</Link>
+					</StyledApplyNowContainer>
 				</GuildIntroduction>
 				<StyledSearchbar>
 					<input
@@ -149,10 +158,84 @@ const GuildListContainer = ({
 
 export default GuildListContainer;
 
-const MemberFormUi = styled.div` 
+const MemberFormUi = styled.div`
 	background-color: #111827;
 	min-height: 100%;
 	min-width: 100%;
+`;
+const GuildPage = styled.div`
+	background-color: #e5e7eb;
+	margin-left: auto;
+	margin-right: auto;
+	padding-top: 100px;
+	width: 55em;
+`;
+const GuildIntroduction = styled.div`
+	color: #36454f;
+	padding-left: 5em;
+	padding-right: 5em;
+	margin-bottom: 5em;
+
+	h1 {
+		font-size: 3.5em;
+		font-weight: 700;
+	}
+	p {
+		font-size: 1.1em;
+		font-weight: 400;
+		line-height: 1.7;
+		padding-bottom: 2.2em;
+	}
+`;
+const StyledApplyNowContainer = styled.div`
+	background-color: #111827;
+	color: #f3f4f6;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+	border-radius: 0.5em;
+	padding: 1em 3.5em;
+
+	width: 725px;
+	box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+
+	h1 {
+		font-size: 2em;
+		padding-bottom: 0.2em;
+	}
+	p {
+		font-size: 1em;
+		padding-bottom: 1em;
+	}
+`;
+const StyledApplyNowButton = styled.button`
+	background-color: rgb(119, 178, 85);
+	border: none;
+	outline: none;
+	border-radius: 20px;
+	padding: 10px 20px;
+
+	a {
+		color: #36454f;
+		font-size: 1.1em;
+		font-weight: 400;
+		text-decoration: none;
+	}
+`;
+const GuildCardContainer = styled.div`
+	background-color: #1f2937;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
+	row-gap: 7px;
+	column-gap: 7px;
+	padding: 10px 0px;
+	margin-top: 30px;
+	margin-left: auto;
+	margin-right: auto;
+	width: 725px;
 `;
 const StyledSearchbar = styled.div`
 	display: flex;
@@ -169,34 +252,6 @@ const StyledSearchbar = styled.div`
 		border-radius: 6px;
 		padding: 5px 250px 5px 10px;
 	}
-`;
-const GuildPage = styled.div`
-	background-color: #e5e7eb;
-	margin-left: auto;
-	margin-right: auto;
-	padding-top: 100px;
-	width: 60em;
-`;
-const GuildIntroduction = styled.div`
-	color: #36454f;
-	padding-left: 5em;
-	padding-right: 5em;
-	margin-bottom: 6em;
-`;
-
-const GuildCardContainer = styled.div`
-	background-color: #1f2937;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-wrap: wrap;
-	row-gap: 7px;
-	column-gap: 7px;
-	padding: 10px 0px;
-	margin-top: 30px;
-	margin-left: auto;
-	margin-right: auto;
-	width: 725px;
 `;
 const Response = styled.div`
 	height: 600px;
